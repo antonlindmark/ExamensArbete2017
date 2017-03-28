8ecdb52707f96f68139345acc7bb3466ce4b5f5a
Anton Lindmark <Anton Lindmark>
2017-03-28 15:49:58 +0200

    working on bluetooth connection

---
+++ OBDAndroid/app/src/main/java/anton/obdandroid/MainActivity.java
@@ -0,0 +1,78 @@
+package anton.obdandroid;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import static android.R.id.list;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int REQUEST_ENABLE_BT=1;
+    private ListView listView;
+    private String selectedFromList;
+    private BluetoothAdapter mBluetoothAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list);
+        bluetoothConnection();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
+
+                String tempString= (String) (listView.getItemAtPosition(myItemInt));
+                String parts[] = tempString.split("-");
+                selectedFromList=parts[1];
+                System.out.println(selectedFromList);
+
+                BluetoothDevice d = mBluetoothAdapter.getRemoteDevice(selectedFromList);
+                ConnectThread t = new ConnectThread(d,mBluetoothAdapter);
+                System.out.println("1");
+                t.start();
+                System.out.println("2");
+            }
+        });
+
+    }
+
+    public void bluetoothConnection(){
+        // Check whether the device has bluetooth
+       mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+        //Makes request for enabliing bluetooth if not enabled
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        ArrayList<String> al = new ArrayList<>() ;
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                al.add(deviceName+"-"+deviceHardwareAddress);
+            }
+        }
+        listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1, al));
+    }
+}
+
+
